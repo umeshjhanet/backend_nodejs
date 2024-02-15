1c2e180014db980b83da22e3a8c89f00220bdc47
Pooja Sharma <umeshjha.net@gmail.com>
2024-02-15 12:38:36 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+var cors = require('cors')
 const mysql = require('mysql2');
-
+app.use(cors())
+ 
+var corsOptions = {
+  origin: 'https://backend-nodejs-nine.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+ 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -25,7 +32,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on https://backend-nodejs-nine.vercel.app:${PORT}`);
 });
 
-app.get('/users', (req, res) => {
+app.get('/users', cors(corsOptions), (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
       if (err) throw err;
       res.json(results);
