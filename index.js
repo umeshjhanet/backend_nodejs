8e0418d6b17842d6398c93a83074e228d17c9148
Pooja Sharma <umeshjha.net@gmail.com>
2024-02-15 12:23:24 +0530

    vercel.json

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ db.connect((err) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://backend-nodejs-nine.vercel.app:${PORT}`);
 });
 
 app.get('/users', (req, res) => {
