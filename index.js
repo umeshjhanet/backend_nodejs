bbd137648b886a1bc4da78df3fb5468f331c3162
Pooja Sharma <umeshjha.net@gmail.com>
2024-02-15 12:02:05 +0530

    myfirstcommit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'nodejs_rest_api',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+
+  
